7d4a47d3a2c600f2194dd05638e8fa7c50b9d045
Aayush Chouhan <aayushchouhanxyz27@gmail.com>
2026-06-19 22:06:38 +0530

    some more thing is add

---
+++ Technical/practice/basic-practice/prac1.py
@@ -0,0 +1,67 @@
+'''a=0b11111
+print(hex(a))
+
+
+x=2.34
+y=-0.9999_990
+z=0.0000
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+import math
+a=-3.2
+print(math.ceil(a))
+print(math.floor(a))
+print(round(a))
+print(abs(a))
+
+x=1.2e2
+print(x)
+
+
+
+
+x= 3+4j
+print(x.real)
+print(x.imag)
+
+a= True
+print(a)
+print(type(a))
+
+print(True+True)
+
+
+import sys
+a = 100323323225532
+b= True
+c="Seema"
+
+k=10
+j=10
+l=10
+
+print(sys.getsizeof(a))
+print(sys.getsizeof(b))
+print(sys.getsizeof(c))
+
+
+print(id(k))
+print(id(j))
+print(id(l))
+
+
+'''
+
+x=True
+y= True
+print(id(x),id(y),sep="\n")
+
+
+
+
